4150c8796c33ba2b60fefc0000c3c820d0ba0823
cmsegui <cmsegui@yahoo.com>
2017-07-31 21:05:59 -0400

    added two other routes but have not connected them

--- app.js
+++ app.js
@@ -1,17 +1,24 @@
-var express = require('express'); // Loading the express module on our server
-var app = express(); // Creates a new instance of express on our server
+const express = require('express'); // Loading the express module on our server
+const app = express(); // Creates a new instance of express on our server
 
 
 app.get("/", function(req, res){
-  // display 'Hello World!'
-  res.send('Hello World!');
+  res.send('Portfolio');
 });
 
-var port = process.env.PORT || 3000; // tells the server where to listen for requests
+app.get('/aboutme', function(req, res) {
+  res.send('About Me');
+});
+
+app.get('/works', function(req, res) {
+  res.send('Works');
+});
+
+const port = process.env.PORT || 3000; // tells the server where to listen for requests
 
 app.listen(port, function() {
   // tells the server where to listen for requests on port 3000
 
-  console.log('hello-express is listening on port ' + port);
+  console.log('Listening on port ' + port);
 }); // actualizing the line above
 
