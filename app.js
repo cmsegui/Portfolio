f1169c3d4dcccbbc9c26af322166672f0adacac0
cmsegui <cmsegui@yahoo.com>
2017-07-31 20:59:31 -0400

    got the local host to work

--- app.js
+++ app.js
@@ -2,8 +2,9 @@ var express = require('express'); // Loading the express module on our server
 var app = express(); // Creates a new instance of express on our server
 
 
-app.get('/', function(req, res) { 
-  // when a request comes in at localhost:3000, it will respond 
+app.get("/", function(req, res){
+  // display 'Hello World!'
+  res.send('Hello World!');
 });
 
 var port = process.env.PORT || 3000; // tells the server where to listen for requests
@@ -13,3 +14,4 @@ app.listen(port, function() {
 
   console.log('hello-express is listening on port ' + port);
 }); // actualizing the line above
+
